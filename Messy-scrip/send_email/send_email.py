4c0203f72c50423dc54b5682407fd58828ae2c72
messyidea <the.mid.sea@gmail.com>
2015-07-03 00:28:24 +0800

    add send email

---
+++ Messy-scrip/send_email/send_email.py
@@ -0,0 +1,34 @@
+# -*- coding: UTF-8 -*-
+'''
+发送txt文本邮件
+copy from：http://www.cnblogs.com/xiaowuyi
+'''
+import smtplib  
+from email.mime.text import MIMEText  
+mailto_list=['xxx@xxx.com'] 
+mail_host="smtp.126.com"  #设置服务器
+mail_user="xxx"    #用户名
+mail_pass="xxx"   #口令 
+mail_postfix="126.com"  #发件箱的后缀
+  
+def send_mail(to_list,sub,content):  
+    me="hello"+"<"+mail_user+"@"+mail_postfix+">"  
+    msg = MIMEText(content,_subtype='plain',_charset='gb2312')  
+    msg['Subject'] = sub  
+    msg['From'] = me  
+    msg['To'] = ";".join(to_list)  
+    try:  
+        server = smtplib.SMTP()  
+        server.connect(mail_host)  
+        server.login(mail_user,mail_pass)  
+        server.sendmail(me, to_list, msg.as_string())  
+        server.close()  
+        return True  
+    except Exception, e:  
+        print str(e)  
+        return False  
+if __name__ == '__main__':  
+    if send_mail(mailto_list,"hello","hello world"):  
+        print "发送成功"  
+    else:  
+        print "发送失败"
