64b233e885527b0edbd5b6564b5155983a95ccea
messyidea <the.mid.sea@gmail.com>
2016-07-01 19:31:59 +0800

    add Proxy-Authenticate header

--- net programming/go-ssl/old-code/main.go
+++ net programming/go-ssl/old-code/main.go
@@ -41,6 +41,7 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		auth := r.Header.Get("Proxy-Authorization")
 		r.Header.Del("Proxy-Authorization")
 		if auth != basicAuthUserPass {
+            w.Header().Set("Proxy-Authenticate", "Basic realm=\"go-proxy\"")
 			http.Error(w, "ProxyAuthRequired", http.StatusProxyAuthRequired)
 			return
 		}
