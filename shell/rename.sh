6163f2e40083708902da876bfe39362260686f36
messyidea <the.mid.sea@gmail.com>
2016-12-15 22:12:30 +0800

    update rename.sh

--- shell/rename.sh
+++ shell/rename.sh
@@ -4,6 +4,10 @@ for file in $filelist
 do 
     if [ "$file" = "rename.sh" ]; then
         continue
+    fi
+    if [ -d $file ]; then
+        echo "$file is a dir, do nothing"
+        continue
     else
         newname=`md5sum $file`
         md5=`echo $newname | awk '{print $1}'`
@@ -12,7 +16,12 @@ do
         # echo $suffix
         newname="$md5"".""$suffix"
         # echo $newname
-        echo "mov $file to $newname"
-        mv $file $newname
+        if [ "$file" = "$newname" ]; then
+            echo "$file do not need to change name"
+            continue
+        else
+            echo "mov $file to $newname"
+            mv $file $newname
+        fi
     fi
 done
